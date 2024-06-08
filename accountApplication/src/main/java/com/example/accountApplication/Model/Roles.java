0d3f36c6fe5970726e86fa794ac8a83177f691bb
Bukunmi15 <oyeoluwabukunmi@gmail.com>
2024-06-08 15:35:30 +0100

    Making my third commit in the project

--- accountApplication/src/main/java/com/example/accountApplication/Model/Roles.java
+++ accountApplication/src/main/java/com/example/accountApplication/Model/Roles.java
@@ -9,6 +9,39 @@ import javax.persistence.*;
 @Table(name = "roles")
 public class Roles implements GrantedAuthority {
 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "role_id")
+
+    private Integer roleId;
+    private String authority;
+
+    public Roles(){
+        super();
+    }
+
+    public Roles (Integer roleId , String authority){
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
 }
 
 
--- accountApplication/src/main/java/com/example/accountApplication/Model/Users.java
+++ accountApplication/src/main/java/com/example/accountApplication/Model/Users.java
@@ -1,8 +1,10 @@
 package com.example.accountApplication.Model;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,34 +13,91 @@ import java.util.Set;
 public class Users implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer user_id;
-
-    @Column(name = "username", unique = true)
+    private Integer userId;
+    @Column(unique = true)
     private String username;
-
-    @Column(name = "password")
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
-            name = "user_role_junction",
-            joinColumns = {@JoinColumn(name = "id")},
+            name="user_role_junction",
+            joinColumns = {@JoinColumn (name="user_id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id")}
     )
 
     private Set<Roles> authorities;
 
-    public Users {
+    public  Users(){
         super();
         authorities = new HashSet<>();
     }
 
-    public Users (Integer userId, String username, String password, Set<Roles> authorities){
-        super();
-        this.user_id =userId;
+
+    public Users(Integer userId, String username, String password, Set<Roles> authorities){
+        this.userId = userId;
         this.username = username;
         this.password = password;
         this.authorities = authorities;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+
+    public void setAuthorities(Set<Roles>authorities){
+        this.authorities = authorities;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    /* If you want account locking capabilities create variables and ways to set them for the methods below */
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
     }
 
 
