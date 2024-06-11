08cfd65211328ecd77706add96571ddd4975f7af
Bukunmi15 <oyeoluwabukunmi@gmail.com>
2024-06-11 14:27:26 +0100

    Adding Files toThe Repository

--- accountApplication/src/main/java/com/example/accountApplication/Repository/UssdAccountRepo.java
+++ accountApplication/src/main/java/com/example/accountApplication/Repository/UssdAccountRepo.java
@@ -3,5 +3,10 @@ package com.example.accountApplication.Repository;
 import com.example.accountApplication.Model.UssdAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UssdAccountRepo extends JpaRepository<UssdAccount,Long> {
+
+    @Override
+    List<UssdAccount> findAllById(Iterable<Long> longs);
 }
